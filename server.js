738ffce2ec613dfa3282212a049315b9cd5b7375
Singh <seezoo@gmail.com>
2017-05-27 19:04:29 -0400

    adding examples to app.user

--- server.js
+++ server.js
@@ -25,9 +25,37 @@ var router = express.Router();      //get an instance of express Router
 // test route to make sure everything is working 
 router.get('/', function(req, res) {
 	res.json({
-		message: 'Hello...Welcome to your webservice'
+		message: 'Hello...Welcome to your webservice.You can get users by saying /webservice/users'
 	});
 });
+
+/*
+This example shows a middleware function with no mount path. The function is executed every time the app receives a request.
+
+var app = express()
+
+app.use(function (req, res, next) {
+  console.log('Time:', Date.now())
+  next()
+})
+*/
+
+/*
+This example shows a middleware function mounted on the /user/:id path. The function is executed for any type of HTTP request on the /user/:id path.
+
+app.use('/user/:id', function (req, res, next) {
+  console.log('Request Type:', req.method)
+  next()
+})
+*/
+
+/*
+This example shows a route and its handler function (middleware system). The function handles GET requests to the /user/:id path.
+
+app.get('/user/:id', function (req, res, next) {
+  res.send('USER')
+})
+*/
 
 //REGISTER OUR ROUTES
 // all of router will be prefixed with /webservice
